cfcf87a0c6e0f58c9608ecc818e0ef4b4b29a5e8
Mielczarek, Lukasz (contracted) <Lukasz.Mielczarek@contracted.pmi.com>
2022-01-29 12:39:31 +0100

    Add basic search input pages

---
+++ src/components/search/SearchForm.js
@@ -0,0 +1,63 @@
+import {useState, useCallback} from "react";
+import InfoStep from "./InfoStep";
+import PreferencesStep from "./PreferencesStep";
+import Error from "../error/Error";
+
+const SearchForm = (callback, deps) => {
+
+    const FormTitles = ['Personal Info', 'Preferences'];
+
+    const [page, setPage] = useState(0);
+    const [formData, setFormData] = useState({
+        height: 0,
+        weight: 0,
+        size: 0,
+        skill: "",
+        ride: ""
+    })
+
+    const PageDisplay = () => {
+        switch (page) {
+            case 0:
+                return <InfoStep formData={formData} setFormData={setFormData}/>
+            case 1:
+                return <PreferencesStep formData={formData} setFormData={setFormData}/>
+            default:
+                return <Error/>
+        }
+    }
+
+    const submit = useCallback(formData => {
+        // fetch()
+        alert(JSON.stringify(formData))
+    })
+
+    return (
+        <div className='search-form'>
+            <div className="progress-bar"/>
+            <div className="form-container">
+                <div className="header">
+                    <h1>{FormTitles[page]}</h1>
+                </div>
+                <div className="body">
+                    <form className="form-horizontal" action="" method="POST" name="" id="">
+                        {PageDisplay()}
+                    </form>
+                </div>
+                <div className="footer">
+                    <button type="button" className="btn btn-dark" onClick={() => setPage(page - 1)}
+                            disabled={page === 0}>Prev
+                    </button>
+                    <button type="button" className="btn btn-dark" onClick={() =>
+                        page === FormTitles.length - 1 ?
+                            submit(formData) :
+                            setPage(page + 1)
+                    }>{page === FormTitles.length - 1 ? "Submit" : "Next"}
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default SearchForm;
